2cf7c0250d15fcccca3eadf4abea0cb472b3a7d7
Javier Bórquez <hi@javier.xyz>
2016-04-16 04:13:20 -0500

    Add twitter button

--- src/HighlightEditor.js
+++ src/HighlightEditor.js
@@ -7,6 +7,8 @@ This sentence has five words. Here are five more words. Five-word sentences are 
 Now listen. I vary the sentence length, and I create music. Music. The writing sings. It has a pleasant rhythm, a lilt, a harmony. I use short sentences. And I use sentences of medium length. And sometimes when I am certain the reader is rested, I will engage him with a sentence of considerable length, a sentence that burns with energy and builds with all the impetus of a crescendo, the roll of the drums, the crash of the cymbals--sounds that say listen to this, it is important.
 
 So write with a combination of short, medium, and long sentences. Create a sound that pleases the reader's ear. Don't just write words. Write music.
+
+-Gary Provost
 `
 
 function getLengthColor(length) {
